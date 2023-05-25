995511902a71efaeb62947244e9cc8e45833b89f
unknown <alexx42nbt@gmail.com>
2023-05-25 07:30:36 +0300

    commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import requests
+from flask import Flask, render_template, request
+import psycopg2
+
+
+app = Flask(__name__)
+conn = psycopg2.connect(database="service_db",
+                        user="postgres",
+                        password="parol",
+                        host="localhost",
+                        port="5432")
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username and password:
+        cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+        records = list(cursor.fetchall())
+        if records:
+            return render_template('account.html', full_name=records[0][1])
+        else:
+            return render_template('error.html',
+                                   err="There's no such user in the database")
+    else:
+        return render_template('error.html',
+                               err="It's impossible not to enter anything")
+
+
+app.run()
